535732a94fe522144b22139fef37123860148797
Rodrigo Pinto Coelho Gomes <41244788+RodPin@users.noreply.github.com>
2020-05-14 21:30:30 -0300

    refactor

--- backend.js
+++ backend.js
@@ -11,45 +11,57 @@ app.use(cors());
 const port = process.env.PORT || 3002;
 
 let count = 1;
-//Basically if we don`t save all the connections we had so far, it will only be possible to send the message to the last client
-//Thats why we create this array of clients
-let connectedClients = [];
 
 //Function to send the counter to the frontEnd
-const sendCounter = (res) => {
-  setTimeout(() => {
+const xSSE = new SSE("/counter");
+const sendCounter = () => {
+  -setTimeout(() => {
     //Send the counter to the client every 3 seconds
-    console.log("Responding counter: " + count);
-    //Here we send to the clients individually (we set it on line 37)
-    for (xClient of connectedClients) {
-      xClient.res.write(`data: ${JSON.stringify({ count })}`);
-      xClient.res.write("\n\n");
-    }
+
+    xSSE.broadCast({ count });
+
     count++;
-    sendCounter(res);
+    sendCounter();
   }, 3000);
 };
 //Here i make the route active
 //If the front end connects to the route it will receive the counter updates
-app.get("/counter", (req, res) => {
-  //Identifying the connected client with an id
-  const clientId = Date.now();
-  //Pushing the client to the connectedClients array
-  console.log("Connected Client: " + clientId);
-  connectedClients.push({ id: clientId, res });
+function SSE(pPath) {
+  if (!pPath) {
+    throw "No endPoint found, you need to pass an endpoint to 'SSE()'";
+  }
+  //Basically if we don`t save all the connections we had so far, it will only be possible to send the message to the last client
+  //Thats why we create this array of clients
+  this.connectedClients = [];
+  app.get(pPath, (req, res) => {
+    this.broadCast = (pObj) => {
+      console.log("Responding counter: ", pObj.count);
+      //Here we send to the clients individually (we set it on line 37)
+      for (xClient of this.connectedClients) {
+        xClient.write(`data: ${JSON.stringify(pObj)}`);
+        xClient.write("\n\n");
+      }
+    };
+    //Identifying the connected client with an id
+    const clientId = Date.now();
+    //Pushing the client to the connectedClients array
+    console.log("Connected Client: " + clientId);
+    this.connectedClients.push(res);
 
-  //Make the connection active
-  res.writeHead(200, {
-    Connection: "keep-alive",
-    "Content-Type": "text/event-stream",
-    "Cache-Control": "no-cache",
+    //Make the connection active
+    res.writeHead(200, {
+      Connection: "keep-alive",
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+    });
+    //When user closes client (navegador) we need to remove it from the connectedClients array
+    req.on("close", () => {
+      console.log(`Disconnected Client: ${clientId}`);
+      this.connectedClients = this.connectedClients.filter((c) => c !== res);
+    });
   });
-  //When user closes client (navegador) we need to remove it from the connectedClients array
-  req.on("close", () => {
-    console.log(`Disconnected Client: ${clientId}`);
-    connectedClients = connectedClients.filter((c) => c.id !== clientId);
-  });
-  sendCounter(res);
-});
+}
+
+sendCounter();
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
